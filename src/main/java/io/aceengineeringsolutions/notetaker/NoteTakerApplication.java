f6ad139085d2fa386150f4695a14924b509b490a
pmarti99 <parkerjmartin883@gmail.com>
2023-06-08 23:14:02 -0600

    initial commit

---
+++ src/main/java/io/aceengineeringsolutions/notetaker/NoteTakerApplication.java
@@ -0,0 +1,13 @@
+package io.aceengineeringsolutions.notetaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteTakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteTakerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/aceengineeringsolutions/notetaker/NoteTakerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.aceengineeringsolutions.notetaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteTakerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
